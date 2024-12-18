3ec1c8b1a31ad4b78997e4004c1265fb9c830c0a
Roberto <93655799+frabetti72@users.noreply.github.com>
2024-12-18 23:49:05 +0100

    introdotto commento di test

--- BFS_Resolution_Sequenziale.c
+++ BFS_Resolution_Sequenziale.c
@@ -148,6 +148,7 @@ int main() {
         {'0','#','#','#','#','0','0'},
         {'0','0','0','0','0','0','0'}
     };
+    //test
     
     Point start, end;
     findStartEnd(maze, &start, &end);
